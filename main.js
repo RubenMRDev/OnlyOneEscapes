271a54a13ca2cdea2e8645d3af50b37c17da4bf0
RubenMRDev <rubenmartinruiz05@gmail.com>
2024-11-06 11:33:32 +0100

    logic created

--- main.js
+++ main.js
@@ -4,17 +4,19 @@ const nombres = [
     "Nicolás Burgos Contreras",
     "Felipe Chacón Montero",
     "Fernando de la Torre Esperon",
+    "Jesús Manuel García Lozano",
     "Alejandro Gómez Ojeda",
     "Pablo Jiménez Menéndez",
     "Mario Lebrero García",
     "Pablo Noria Gómez",
-    "Mauricio Nicolás Ortiz",
+    "Mauricio Nicolas Ortiz",
     "Adrián Pérez Agredano",
     "Jairo Saborito Franco",
     "Judith Tamayo Balogh",
     "Samuel Utrilla Núñez",
-    "Rubén Martín Ruiz"
+    "Ruben Martin Ruiz"
 ];
+
 
 
 function mezclarArray(array) {
@@ -23,8 +25,6 @@ function mezclarArray(array) {
         [array[i], array[j]] = [array[j], array[i]];
     }
 }
-
-
 
 function crearParejas(nombres){
 
@@ -38,79 +38,161 @@ for (let i = 0; i < nombres.length; i += 2) {
 }
 return parejas;
 }
-mezclarArray(nombres);
-let jugadores = crearParejas(nombres);
 
-// for(let i=0;i<parejas.length;i++){
-//     document.getElementById("players").innerHTML += parejas[i].join(" y ") + "<br>";
-// }
+mezclarArray(nombres);
+
+
 
 let muertos=[];
 let vivos=[];
 let jugando=[];
 let ganadores= [];
 
-function empezarPartida(jugador1,jugador2){
-    let dado1 = Math.floor(Math.random()*6+1);
-    let dado2 = Math.floor(Math.random()*6+1);
-    if(dado1==dado2){
-        console.log("Han salido dados repetidos")
-        empezarPartida(jugador1,jugador2);
+vivos = nombres;
+
+
+// VARIABLES DEL DOM
+const player1dice= document.getElementById("player1dice");
+const playe2dice= document.getElementById("player2dice");
+const player1= document.getElementById("player1");
+const player2= document.getElementById("player2");
+const roundcount= document.getElementById("roundcount");
+const aliveplayers= document.getElementById("aliveplayers");
+const deathplayers= document.getElementById("deathplayers");
+
+let step =0;
+let i = 0;
+let intervalo = 100; // Tiempo inicial en milisegundos
+
+
+function tirarDado(dado1HTML, dado2HTML,jugadores) {
+    return new Promise((resolve) => {
+        let dado1 = Math.floor(Math.random() * 6 + 1);
+        let dado2 = Math.floor(Math.random() * 6 + 1);
+        dado1HTML.innerHTML = "<span>" + dado1 + "</span>";
+        dado2HTML.innerHTML = "<span>" + dado2 + "</span>";
+
+        i++;
+        intervalo *= 1.3;
+
+        if (i >= 10) { 
+            dado1HTML.innerHTML = "<h1>" + dado1 + "</h1>";
+            dado2HTML.innerHTML = "<h1>" + dado2 + "</h1>";
+            intervalo = 100;
+            i = 0;
+            
+            // Comprobar si los números son iguales
+            if (dado1 === dado2) {
+                // Si los dados son iguales, esperar 1 segundo antes de reiniciar
+                setTimeout(() => {
+                    // Llamar de nuevo a la función para continuar tirando los dados
+                    resolve(tirarDado(dado1HTML, dado2HTML,jugadores));
+                }, 1000);  // Espera de 1 segundo (1000 ms)
+            } else {
+
+                if(dado1>dado2){
+                    ganadores.push(jugadores[step][0]);
+                    aliveplayers.innerHTML+=jugadores[step][0] + "<br>";
+                    muertos.push(jugadores[step][1])
+                    deathplayers.innerHTML+=jugadores[step][1] + "<br>";
+                    step++;
+                }else{
+                    ganadores.push(jugadores[step][1]);
+                    aliveplayers.innerHTML+=jugadores[step][1] + "<br>";
+                    muertos.push(jugadores[step][0])
+                    deathplayers.innerHTML+=jugadores[step][0] + "<br>";
+                    step++;
+                }
+                resolve();
+            }
+        } else {
+            setTimeout(() => resolve(tirarDado(dado1HTML, dado2HTML,jugadores)), intervalo);
+        }
+    });
+}
+function boss(dado1HTML, dado2HTML,jugadores) {
+    return new Promise((resolve) => {
+        let dado1 = Math.floor(Math.random() * 6 + 1);
+        let dado2 = Math.floor(Math.random() * 6 + 1);
+        dado1HTML.innerHTML = "<span>" + dado1 + "</span>";
+        dado2HTML.innerHTML = "<span>" + dado2 + "</span>";
+
+        i++;
+        intervalo *= 1.3;
+
+        if (i >= 10) { 
+            dado1HTML.innerHTML = "<h1>" + dado1 + "</h1>";
+            dado2HTML.innerHTML = "<h1>" + dado2 + "</h1>";
+            intervalo = 100;
+            i = 0;
+            
+            // Comprobar si los números son iguales
+            if (dado1 === dado2) {
+                // Si los dados son iguales, esperar 1 segundo antes de reiniciar
+                setTimeout(() => {
+                    // Llamar de nuevo a la función para continuar tirando los dados
+                    resolve(tirarDado(dado1HTML, dado2HTML,jugadores));
+                }, 1000);  // Espera de 1 segundo (1000 ms)
+            } else {
+
+                if(dado1>dado2){
+                    ganadores.push(jugadores[step][0]);
+                    aliveplayers.innerHTML+=jugadores[step][0] + "<br>";
+                    step++;
+                }else{
+                    muertos.push(jugadores[step][0])
+                    deathplayers.innerHTML+=jugadores[step][0] + "<br>";
+                    step++;
+                }
+                resolve();
+            }
+        } else {
+            setTimeout(() => resolve(tirarDado(dado1HTML, dado2HTML,jugadores)), intervalo);
+        }
+    });
+}
+
+async function ejecutarRonda(jugadores) {
+    for (let i = 0; i < jugadores.length; i++) {
+            if(jugadores[i][1]==undefined){
+                player1.innerHTML=jugadores[i][0]
+                player2.innerHTML="PALOMA"
+                await tirarDado(player1dice, playe2dice,jugadores);
+            }else{
+                // Esperar a que termine la tirada del dado de cada jugador antes de continuar
+                player1.innerHTML=jugadores[i][0]
+                player2.innerHTML=jugadores[i][1]
+                await tirarDado(player1dice, playe2dice,jugadores);
+            }
+
+        
+        
+        // Esperar 1 segundo antes de pasar al siguiente jugador
+        await new Promise(resolve => setTimeout(resolve, 2000));  // Espera de 1 segundo
+
+    }
+
+    if(ganadores.length ==1){
+        alert("el ganador es "+ ganadores[0])
     }else{
-        if(dado1>dado2){
-            console.log(jugador2 + " mato a " + jugador1)
-            muertos.push(jugador1);
-            ganadores.push(jugador2);
-        }else{
-            console.log(jugador1 + " mato a " + jugador2)
-            muertos.push(jugador2);
-            ganadores.push(jugador1);
-        }
+
+        vivos= ganadores;
+        ganadores=[];
+        document.getElementById("playbutton").style.display="inline";
+        step=0;
     }
+
 }
 
-function boss(jugador1){
-    let dado1 = Math.floor(Math.random()*6+1);
-    let dado2 = Math.floor(Math.random()*6+1);
-    if(dado1==dado2){
-        console.log("Han salido dados repetidos")
-        boss(jugador1);
-    }else{
-        if(dado1>dado2){
-            console.log(jugador1 + " murio a manos de Paloma")
-            muertos.push(jugador1);
-        }else{
-            console.log(jugador1 + " sobrevivio a Paloma.")
-            ganadores.push(jugador1);
-        }
-    }
+
+let roundNumber=0;
+
+function startRound(){
+    roundNumber++;
+    roundcount.innerHTML="RONDA " + roundNumber;
+    aliveplayers.innerHTML="";
+    document.getElementById("playbutton").innerHTML="Next Round!";
+    document.getElementById("playbutton").style.display="none";
+
+    ejecutarRonda(crearParejas(vivos))
 }
-
-function ejecutarRonda(jugadores){
-    for(let i=0;i<jugadores.length;i++){
-        if(jugadores[i][1] === undefined){
-
-            boss(jugadores[i][0])
-
-        }else{
-            empezarPartida(jugadores[i][0],jugadores[i][1]);
-        }
-    }
-}
-
-console.log(jugadores)
-
-jugando = jugadores;
-
-while(jugando.length>1){
-    console.log("== NUEVA RONDA ==")
-    ejecutarRonda(jugadores)
-    console.log("MUERTOS")
-    console.log(muertos)
-    console.log("GANADORES")
-    console.log(ganadores)
-    jugando= ganadores;
-    mezclarArray(jugando);
-    jugadores = crearParejas(ganadores);
-    ganadores = [];
- }
