7849ef4bde6836126035ea23f7063f1caab7f57e
jthamayo <jtamayobalogh@gmail.com>
2024-11-28 10:59:09 +0100

    add link to hosted images and move goblin data to separate file

---
+++ goblin.js
@@ -0,0 +1,142 @@
+export const goblins = {
+  1: {
+    color: "black",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786869/idleBlack_jd3lw4.gif",
+      death: "",
+    },
+  },
+  2: {
+    color: "black-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786869/idleBlackYellow_rsdo7j.gif",
+      death: "",
+    },
+  },
+  3: {
+    color: "blue",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786868/iddlePurple_n6gal5.gif",
+      death: "",
+    },
+  },
+  4: {
+    color: "blue-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786866/idlePurpleYellow_gx5qpi.gif",
+      death: "",
+    },
+  },
+  5: {
+    color: "brown",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786858/idleBrown_zomgdq.gif",
+      death: "",
+    },
+  },
+  6: {
+    color: "brown-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786859/idleBrownYellow_vreuly.gif",
+      death: "",
+    },
+  },
+  7: {
+    color: "green",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786859/idleGreen_u5glu0.gif",
+      death: "",
+    },
+  },
+  8: {
+    color: "green-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786860/idleGreenYellow_whdt1d.gif",
+      death: "",
+    },
+  },
+  9: {
+    color: "olive",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786862/idleOlive_qb2e7f.gif",
+      death: "",
+    },
+  },
+  10: {
+    color: "olive-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786861/idleOliveYellow_hweqrs.gif",
+      death: "",
+    },
+  },
+  11: {
+    color: "orange",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786862/idleOrange_xvmowk.gif",
+      death: "",
+    },
+  },
+  12: {
+    color: "orange-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786863/idleOrangeYellow_nttaz2.gif",
+      death: "",
+    },
+  },
+  13: {
+    color: "pink",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786863/idlePink_mohhl7.gif",
+      death: "",
+    },
+  },
+  14: {
+    color: "pink-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786864/idlePinkYellow_jwgu3g.gif",
+      death: "",
+    },
+  },
+  15: {
+    color: "pistacho",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786865/idlePistacho_avwf7w.gif",
+      death: "",
+    },
+  },
+  16: {
+    color: "pistacho-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786866/idlePistachoYellow_fzbgn2.gif",
+      death: "",
+    },
+  },
+  17: {
+    color: "purple",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786868/iddlePurple_n6gal5.gif",
+      death: "",
+    },
+  },
+  18: {
+    color: "purple-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786866/idlePurpleYellow_gx5qpi.gif",
+      death: "",
+    },
+  },
+  19: {
+    color: "red",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786867/idleRed_s4it4j.gif",
+      death: "",
+    },
+  },
+  20: {
+    color: "red-yellow",
+    animations: {
+      idle: "https://res.cloudinary.com/ddguqr8l8/image/upload/v1732786241/idleRedYellow_hhidwt.gif",
+      death: "",
+    },
+  },
+};
